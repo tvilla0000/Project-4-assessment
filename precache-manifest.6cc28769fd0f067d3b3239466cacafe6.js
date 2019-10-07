c2c247728139c2ec0847c88aa114a9d1e35b5af6
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-07 15:14:22 -0700

    Updates

---
+++ precache-manifest.6cc28769fd0f067d3b3239466cacafe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb54dfe993249cd231e542973e5ca6ad",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/index.html"
+  },
+  {
+    "revision": "b093297d8493bf13e54b",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/css/main.d21d3fb8.chunk.css"
+  },
+  {
+    "revision": "bfc19d56d2152622b9fb",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/2.cf7f5447.chunk.js"
+  },
+  {
+    "revision": "b093297d8493bf13e54b",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/main.74edb5bd.chunk.js"
+  },
+  {
+    "revision": "cd2a6c474f5ac56984bc",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/runtime-main.8fe3524a.js"
+  }
+]);
